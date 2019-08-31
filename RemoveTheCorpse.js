ac1fa588b4320397db43910843edb9f4545329c6
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:23:53 -0700

    Update RemoveTheCorpse.js
    Corrected Venmo link

--- RemoveTheCorpse.js
+++ RemoveTheCorpse.js
@@ -4,7 +4,7 @@ Removes tokens marked "dead" from the Turn Tracker
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var RemoveTheCorpse = RemoveTheCorpse || (function () {
